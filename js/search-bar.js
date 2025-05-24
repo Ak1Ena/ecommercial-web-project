2e81306d727c3f9f3d9b487b4a8cf8acd932a7ac
Siraphop Khatchamat <poomzahathothai@gmail.com>
2025-05-24 10:52:13 +0700

    sreach-bar

---
+++ js/search-bar.js
@@ -0,0 +1,31 @@
+// LINE 55 list of Prodyct 
+const searchButton = document.getElementById('search-button');
+
+searchButton.addEventListener('click', searchProducts);
+
+function searchProducts() {
+    
+    const searchInput = document.getElementById('search-input').value.trim();
+    const productList = document.getElementById('products-list');
+    const products = productList.children;
+    const searchTerm = searchInput.toLowerCase();
+
+    if (searchInput === '') {
+        for (let i = 0; i < products.length; i++) {
+            products[i].style.display = '';
+        }
+        return;
+    }
+    for (let i = 0; i < products.length; i++) {
+        const product = products[i];
+        const name = product.querySelector('.product__item__text h6 a').textContent.toLowerCase();
+
+        if (name.includes(searchTerm)) {
+            product.style.display = '';
+        } else {
+            product.style.display = 'none';
+        }
+    }
+
+}
+
