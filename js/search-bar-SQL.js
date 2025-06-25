e19bab58c25a4a1939f16168eb20ae0de4e64d87
Siraphop Khatchamat <poomzahathothai@gmail.com>
2025-06-25 22:30:52 +0700

    Fix add product when search

--- js/search-bar-SQL.js
+++ js/search-bar-SQL.js
@@ -66,7 +66,7 @@ function searchProducts(searchInput) {
                                             <ul class="product__item__pic__hover">
                                                 <li><a href="#"><i class="fa fa-heart"></i></a></li>
                                                 <li><a href="#"><i class="fa fa-retweet"></i></a></li>
-                                                <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
+                                                <li><a href="#" data-product-id="${product.id}"><i class="fa fa-shopping-cart"data-product-id="${product.id}"></i></a></li>
                                             </ul>
                                         </div>
                                         <div class="product__discount__item__text">
